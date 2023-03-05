151c00419e7645eced46cef1bf95483eb19723f2
mongarerobert3 <mongarerobert3@gmail.com>
2023-03-05 14:35:29 +0300

    First API

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+import express from 'express';
 import router from './routes/index';
 
-const express = require('express');
-// create the Express server
-const app = express();
-const port = process.env.PORT || 5000;
+const port = parseInt(process.env.PORT, 10) || 5000;
 
-router(app);
+const app = express();
+
+app.use(express.json());
+app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`server running on port ${port}`);
 });
+
+export default app;
