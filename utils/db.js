089f08d20e26afc61742d9f6798280d0f9ef31f6
mongarerobert3 <mongarerobert3@gmail.com>
2023-03-05 12:36:52 +0300

    MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -28,6 +28,42 @@ export class DBClient {
     return myCollection.countDocuments();
   }
 
+  async userExists(email) {
+    /* returns true if the user with the given email exists */
+    const myDB = this.myClient.db();
+    const myCollection = myDB.collection('users');
+    return myCollection.findOne({ email });
+  }
+
+  async newUser(email, passwordHash) {
+    /* creates a new user with the given email and passwordHash */
+    const myDB = this.myClient.db();
+    const myCollection = myDB.collection('users');
+    return myCollection.insertOne({ email, passwordHash });
+  }
+
+  async filterUser(filters) {
+    const myDB = this.myClient.db();
+    const myCollection = myDB.collection('users');
+    if ('_id' in filters) {
+      // eslint-disable-next-line no-param-reassign
+      filters._id = ObjectId(filters._id);
+    }
+    return myCollection.findOne(filters);
+  }
+
+  async filterFiles(filters) {
+    const myDB = this.myClient.db();
+    const myCollection = myDB.collection('files');
+    const idFilters = ['_id', 'userId', 'parentId'].filter((prop) => prop in filters && filters[prop] !== '0');
+    idFilters.forEach((i) => {
+      // eslint-disable-next-line no-param-reassign
+      filters[i] = ObjectId(filters[i]);
+    });
+    return myCollection.findOne(filters);
+  }
+}
+
 const dbClient = new DBClient();
 
 export default dbClient;
